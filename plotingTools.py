09f2c2fd3ad45025a54d6911cc1a2d4a936a75c6
RicardoS99 <ricardojdsantos@outlook.pt>
2022-05-26 15:27:04 +0100

    Starting plottingTools

---
+++ plotingTools.py
@@ -0,0 +1,11 @@
+from A4_reduced import model1
+
+from cosmoTransitions import generic_potential
+from cosmoTransitions import transitionFinder as tf
+from cosmoTransitions import pathDeformation as pd
+
+import numpy as np
+
+from scipy.interpolate import CubicSpline
+
+import matplotlib.pyplot as plt
